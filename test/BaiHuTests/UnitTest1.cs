721fa91d32e5079bdfaa2eb7c6163d46862a8173
Thomas Tacke <thomas@tacke.email>
2021-11-24 19:09:20 +0100

    Init of Project (#1)
    - Added Projects
        - BaiHu
        - QingLong
        - XuanWu
    - Added Test Projects
        - BaiHuTests
        - QingLongTests
        - XuanWuTests
    - Added
        - .github/CODEOWNERS
        - .gitignore

---
+++ test/BaiHuTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace BaiHuTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ test/QingLongTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace QingLongTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
